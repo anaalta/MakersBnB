c45c0eedb1a3ece5cb3a5102671501b098035944
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-05-15 18:09:28 +0100

    NBG for feature test sign up and log in

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require_relative './data_mapper_setup.rb'
 require_relative './models/user'
 
 class MakersBnB < Sinatra::Base
-    enable :sessions
+  enable :sessions
 
   get '/' do
     erb :index
@@ -16,24 +16,29 @@ class MakersBnB < Sinatra::Base
   end
 
   post '/users' do
-    @user = User.create(email: params[:email], first_name: params[:first_name], last_name: params[:last_name], password: params[:password])
+    @user = User.create(email:      params[:email],
+                        first_name: params[:first_name],
+                        last_name:  params[:last_name],
+                        password:   params[:password])
     erb :home
   end
- 
+
   get '/sessions/new' do
     erb :sessions
   end
 
   post '/sessions' do
     user = User.authenticate(params[:email], params[:password])
-    p params
     session[:user_id] = user.id
+    p params
+    p session
     redirect to '/home'
   end
 
   get '/home' do
-    "successfull logged in"
+    "Welcome, test@example.com"
   end
 
   run! if app_file == $0
+
 end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,16 +1,11 @@
-
-feature "User sign in" do
+feature 'User sign in' do
 
   scenario 'user entering his credentials' do
-      sign_in(email: 'email@com', password: '12345')
-      expect(page).to have_content "Welcome, email@com"
+    sign_up  
+    sign_in
+    expect(page).to have_content "Welcome, test@example.com"
   end
 
-  def sign_in(email:, password:)
-    visit "/sessions/new"
-    fill_in :email, with: 'email@com'
-    fill_in :password, with: '12345'
-    click_on 'Sign in'
-  end
+
   
 end
