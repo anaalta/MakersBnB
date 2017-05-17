7c0ef6e7e4042833f6630a12cb9b4ea596755045
anaalta <suciuanamaria@yahoo.com>
2017-05-17 12:12:09 +0200

    available dates test

---
+++ spec/features/user_sets_available_dates_spec.rb
@@ -0,0 +1,17 @@
+feature 'select dates when property is available' do
+
+  scenario 'user selects available interval' do
+    sign_up
+    sign_in
+    click_button 'Add your space here!'
+    expect(current_path).to eq ('/spaces/new')
+    fill_in :property_name, with: 'my property'
+    fill_in :description, with: 'nice cosy place'
+    fill_in :price_per_night, with: '35'
+    fill_in :available_from, with: '25/05/2017'
+    fill_in :available_until, with: '22/06/2017'
+    click_button 'Submit'
+    expect(current_path).to eq ('/dashboard')
+    expect(page).to have_content :property_name, :description, :price_per_night, :available_from, :available_until
+  end
+end
