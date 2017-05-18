68b8b32101edbac3fe0dee3f20431891e09166a0
danielemanuel <daniel.costea@vkernel.ro>
2017-05-18 17:14:53 +0100

    Feature test for booking confirmation works

--- spec/features/user_can_book_a_place_spec.rb
+++ spec/features/user_can_book_a_place_spec.rb
@@ -19,4 +19,25 @@ feature 'Users can book spaces' do
     click_button 'Confirm booking'
     #expect(page).to have_content ('Thank you for your booking!')
   end
+
+  it 'displays a confirmation of the booking' do
+    sign_up
+    sign_in
+    click_on 'Add your space here!'
+    fill_in :property_name, with: 'my property'
+    fill_in :description, with: 'nice cosy place'
+    fill_in :price_per_night, with: '35'
+    fill_in :available_from, with: '25/05/2017'
+    fill_in :available_until, with: '22/06/2017'
+    click_button 'Submit'
+    expect(current_path).to eq ('/dashboard')
+    within"ol#listings" do
+      click_button "Book Me"
+    end
+    fill_in :start_date, with: '01/01/2017'
+    fill_in :end_date, with: '10/01/2017'
+    click_on 'Confirm booking'
+    expect(page).to have_content('Your booking was succesfull')
+  end
+
 end
