0346203d9212c8949fb91d13f263da80017fdb0f
anaalta <suciuanamaria@yahoo.com>
2017-05-18 13:27:23 +0200

    added form for booking a place

--- spec/features/user_can_book_a_place_spec.rb
+++ spec/features/user_can_book_a_place_spec.rb
@@ -2,13 +2,18 @@ feature 'Users can book spaces' do
   scenario 'a user books a listed place' do
     sign_up
     sign_in
-    save_and_open_page
+    click_on 'Add your space here!'
+    fill_in :property_name, with: 'my property'
+    fill_in :description, with: 'nice cosy place'
+    fill_in :price_per_night, with: '35'
+    fill_in :available_from, with: '25/05/2017'
+    fill_in :available_until, with: '22/06/2017'
+    click_button 'Submit'
+    expect(current_path).to eq ('/dashboard')
     within"ol#listings" do
       click_button "Book Me"
-      find('#id_of_element').click
     end
     expect(page).to have_content :property_name
-    click_button 'Book this property'
     fill_in :start_date, with: '21/08/2017'
     fill_in :end_date, with: '24/08/2017'
     click_button 'Confirm booking'
