a1c1c70f14cd75dcc06458a9aa9448af0bc768c9
danielemanuel <daniel.costea@vkernel.ro>
2017-05-18 17:56:20 +0100

    Available dates for the night

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -23,3 +23,19 @@ def wrong_sign_up
   fill_in :password, with: '12345'
   fill_in :password_confirmation, with: 'wrong'
 end
+
+def bookings
+  sign_up
+  sign_in
+  click_on 'Add your space here!'
+  fill_in :property_name, with: 'my property'
+  fill_in :description, with: 'nice cosy place'
+  fill_in :price_per_night, with: '35'
+  fill_in :available_from, with: '25/05/2017'
+  fill_in :available_until, with: '22/06/2017'
+  click_button 'Submit'
+  expect(current_path).to eq ('/dashboard')
+  within"ol#listings" do
+    click_button "Book Me"
+  end
+end
